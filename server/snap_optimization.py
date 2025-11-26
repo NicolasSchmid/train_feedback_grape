ea9f615c46b69c0249dff74f7b5f617ffdeee9f2
DGX user <nicolas.schmid.6035@gmail.com>
2025-11-26 08:57:53 +0000

    snap optimization with PPO (not converging properly yet)

---
+++ server/snap_optimization.py
@@ -0,0 +1,153 @@
+import nvtx
+import sys
+import numpy as np
+
+from stable_baselines3 import PPO
+
+from dgx_suite.dgx_environment import DgxEnvironment
+from dgx_suite.dgx_parameter_space import DgxActionSpace, DgxObservationSpace
+from dgx_suite.dgx_observer import Observer
+from dgx_suite.opnic_utils import patch_opnic_wrapper
+
+import argparse
+
+from stable_baselines3.common.callbacks import BaseCallback
+SEED = 5
+class BestActionCallback(BaseCallback):
+    def __init__(self, verbose=0):
+        super().__init__(verbose)
+        self.best_reward = -float("inf")
+        self.best_action = None
+
+    def _on_step(self) -> bool:
+        # Each rollout sample is in self.locals['rewards'] and self.locals['actions']
+        rewards = self.locals.get("rewards", None)
+        actions = self.locals.get("actions", None)
+
+        if rewards is None or actions is None:
+            return True
+
+        # PPO samples batches, so loop through collected minibatch
+        for r, a in zip(rewards, actions):
+            if r > self.best_reward:
+                self.best_reward = float(r)
+                self.best_action = a.copy()
+
+        return True
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Train an agent in a DGX environment.")
+    parser.add_argument('--total_timesteps', type=int, default=700, help='Total number of timesteps for training')
+    parser.add_argument('--learning_rate', type=float, default=5e-4, help='Learning rate for the TD3 agent')
+    parser.add_argument('--verbosity', type=int, default=1, choices=[0, 1, 2], help='Verbosity level (0: no output, 1: info, 2: debug)')
+    parser.add_argument('--device', type=str, default='cuda', choices=['cpu', 'cuda'], help='Device to use for training (cpu or cuda)')
+    parser.add_argument('--learning_starts', type=int, default=100, help='Number of steps of exploration before learning starts')
+    parser.add_argument('--batch_size', type=int, default=256, help='Number of samples from the replay buffer used for each gradient update')
+    parser.add_argument('--train_freq', type=int, default=1, help='Determines how often the model updates its parameters based on the number of steps in the environment')
+    parser.add_argument('--sigma_end_as_scale', type=float, default=1., help='Ending action noise std. dev is sigma_start * sigma_end_as_scale')
+    parser.add_argument('--observer', type=str, help='Serialized base64 message packed string of a Observer instance')
+    parser.add_argument('--action_space', type=str, help='Serialized base64 message packed string of a DgxActionSpace instance')
+    parser.add_argument('--observation_space', type=str, help='Serialized base64 message packed string of a DgxActionSpace instance')
+    parser.add_argument('--path_to_python_wrapper', type=str, help='Path to python-wrapper source code.')
+    parser.add_argument('--force_recompile_python_wrapper', action=argparse.BooleanOptionalAction, help='Whether to force-recompile the python wrapper')
+
+    return parser.parse_args()
+
+
+
+if __name__ == "__main__":
+    # Parse command line arguments
+    args = parse_arguments()
+
+    # Deserialize environment-shape objects
+    observer = Observer.deserialize(args.observer)
+    action_space = DgxActionSpace.deserialize(args.action_space)
+    observation_space = DgxObservationSpace.deserialize(args.observation_space)
+
+    # Re-compile the python OPNIC wrapper according to the shape of the environment
+    patch_opnic_wrapper(action_space, observation_space, args.path_to_python_wrapper, args.force_recompile_python_wrapper)
+
+    sys.path.append(f"{args.path_to_python_wrapper}/wrapper/build/python")
+    import opnic_wrapper
+
+    # Create environment with ability to exchange information over DGX
+    dgx_env = DgxEnvironment(
+        observer=observer,
+        action_space=action_space,
+        observation_space=observation_space,
+        opnic_wrapper=opnic_wrapper,
+        verbosity=args.verbosity
+    )
+
+    # Add artifical noise to actions to assist learning
+    sigma_start = np.array([action.sigma for action in action_space.parameters])
+    # action_noise = DecayingNormalActionNoise(
+    #     sigma_start,
+    #     sigma_end_as_scale=args.sigma_end_as_scale,
+    #     total_timesteps=args.total_timesteps
+    # )
+
+    
+    class EntropyDecayCallback(BaseCallback):
+        def __init__(self, initial_ent_coef=0.1, final_ent_coef=0.001, decay_rate=0.999, verbose=0):
+            super().__init__(verbose)
+            self.initial = initial_ent_coef
+            self.final = final_ent_coef
+            self.decay = decay_rate
+
+        def _on_step(self) -> bool:
+            # Update ent_coef
+            new_ent = max(self.final, self.model.ent_coef * self.decay)
+            self.model.ent_coef = new_ent
+            return True
+    
+    # Define the reinforcement learning agent
+    model = PPO(
+        policy='MlpPolicy',
+        env=dgx_env,
+        gamma=1,
+        seed=SEED,
+        # action_noise=action_noise,
+        learning_rate=args.learning_rate,
+        batch_size=args.batch_size,
+        # train_freq=args.train_freq,
+        n_steps=args.train_freq,
+        # learning_starts=args.learning_starts,
+        verbose=args.verbosity,
+        device=args.device,
+        ent_coef=0.05
+    )
+
+    # Train the agent
+    with nvtx.annotate("Model Learn"):
+        model.learn(total_timesteps=args.total_timesteps,callback=EntropyDecayCallback(
+        initial_ent_coef=0.05,
+        final_ent_coef=0.001,
+        decay_rate=0.9
+    ))
+
+    # Evaluate the agent
+    action, _ = model.predict(observation_space.target_state, deterministic=True)
+    obs, reward, done, _, _ = dgx_env.step(action)
+
+    print(f"Sampled final action: {action}, got reward: {reward}")
+    
+    # best_cb = BestActionCallback()
+
+    # with nvtx.annotate("Model Learn"):
+    #     model.learn(
+    #         total_timesteps=args.total_timesteps,
+    #         callback=best_cb
+    #     )
+
+    # # Evaluate the agent
+    # action, _ = model.predict(observation_space.target_state, deterministic=True)
+    # obs, reward, done, _, _ = dgx_env.step(action)
+
+    # print("==== Best action during training ====")
+    # print("Action:", best_cb.best_action)
+    # print("Reward:", best_cb.best_reward)
+
+
+    dgx_env.close()
